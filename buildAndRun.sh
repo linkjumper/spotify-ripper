83cedb05fc6e165fee168dcc9dfc6c1cd60d5c23
Mateusz Jaje <mateuszjaje@gmail.com>
2016-10-13 22:35:50 +0200

    updated build script

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
+CONTAINER_ID=spotify-ripper
+A=`docker inspect -f {{.State.Running}} ${CONTAINER_ID}`
+B=`docker inspect -f {{.State.Dead}} ${CONTAINER_ID}`
+echo "'$A' '$B' '$?'"
+if [ "$A" = "true" ]; then
+    echo "Docker $CONTAINER_ID is running, killing them..."
+    docker kill ${CONTAINER_ID}
+else
+    echo "Docker $CONTAINER_ID not found."
+fi
+sleep 3
+if [ "$B" != "true" ]; then
+    echo "Docker $CONTAINER_ID image exists, removing them..."
+    docker rm ${CONTAINER_ID}
+else
+    echo "Docker image $CONTAINER_ID not found."
+fi
+sleep 1
 docker build -t morgaroth/spotify-ripper .
-docker kill spotify-ripper
-docker rm spotify-ripper
-docker run --name spotify-ripper -v docker_config:/ripper/config -v test:/ripper/songs morgaroth/spotify-ripper:latest
+docker run -d -e user=$(whoami) -e pass="$1" --name ${CONTAINER_ID} -v $(pwd)/docker_config:/ripper/config -v $(pwd)/songs:/ripper/songs morgaroth/spotify-ripper:latest
+echo "Docker fired! Now listening logs..."
+docker logs -f ${CONTAINER_ID}
